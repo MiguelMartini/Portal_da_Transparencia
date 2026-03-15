e6b1c48868bea3d4212e8165afb4ba33b7a9d965
MiguelMartini <miguelkaulingm21@gmail.com>
2026-03-15 11:46:43 -0300

    Feat: Estrutura MainContent

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import MainContent from './MainContent/MainContent'
 
 function Home() {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col'>
       <Header/>
+      <div className='flex-1'>
+        <MainContent/>
+      </div>
       <Footer/>
     </div>
   )
---
+++ src/pages/Home/MainContent/Simulacao.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Simulacao() {
+  return (
+    <div>Simulacao</div>
+  )
+}
+
+export default Simulacao
